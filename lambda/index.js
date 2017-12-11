102ef606d49b3c0e36a7c302f2833fec39cabb84
Alexander Belyaev <alexbel@lavabit.com>
2017-12-11 13:04:56 -0800

    Refactor index.js

--- lambda/index.js
+++ lambda/index.js
@@ -11,16 +11,28 @@ const URL = process.env.URL;
 
 const maxAge = 14 * 24 * 60 * 60;
 
-exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+|null)x(\d+|null)\/(.*)/);
 
-  if (match === null){
+exports.handler = function(event, context, callback) {
+
+  var redirectBack = function(){
+    callback(null, {
+      statusCode: '301',
+      headers: {'location': `${URL}/${key}`},
+      body: ''
+    })
+  };
+  var notFound = function(){
     callback(null, {
       statusCode: '404',
-      headers: {'location': `${URL}/${key}`},
-      body: '',
+      body: ''
     })
+  };
+
+  const key = event.queryStringParameters.key;
+  var match = key.match(/(\d+|null)x(\d+|null)\/(.*)/);
+
+  if (match === null){
+    notFound();
     return false;
   }
 
@@ -42,6 +54,11 @@ exports.handler = function(event, context, callback) {
      contentType = 'image/png';
   }
 
+  if (!['png', 'jpeg', 'gif'].includes(format)){
+    notFound();
+    return false;
+  }
+
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
@@ -57,11 +74,6 @@ exports.handler = function(event, context, callback) {
         Key: key,
       }).promise()
     )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
+    .then(() => redirectBack())
     .catch(err => callback(err))
 }
